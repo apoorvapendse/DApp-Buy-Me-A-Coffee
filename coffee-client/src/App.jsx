fc64f43cc4481d1512265c150f32adcb581f5857
apoorvapendse <apoorvavpendse@gmail.com>
2023-08-18 16:05:03 +0530

    added withdraw function call for the user

--- coffee-client/src/App.jsx
+++ coffee-client/src/App.jsx
@@ -6,26 +6,34 @@ import AdminPage from './AdminPage';
 
 
 const App = () => {
-
-  const [accounts, setAccounts] = useState([]);
+  const [account,setAccount] = useState()
   const[purchases,setPurchases] = useState([])
   const[web3,setWeb3] = useState()
   const[coffeeContract,setCoffeeContract] = useState()
   const [coffeeCost,setCoffeeCost] = useState();
   const contractAddress = "0x5913461443aCE649AcDba032650b3f31003c6E5c"
   const[email,setEmail] = useState();
+  const creatorAddress = "0xB057c56115080DD24571cb54751E090f49C363D9";
   
   console.log(email)
+  const fetchAccounts = async()=>{
+    const userAccounts = await web3.eth.getAccounts();
+    setAccount(userAccounts[0]);
+    
+  }
+  fetchAccounts();
   useEffect(() => {
     
+
     if(window.ethereum){
+      
+    
       window.ethereum.request({method:"eth_requestAccounts"})
       .then(()=>{
         const web3_Instance = new Web3(window.ethereum);
         setWeb3(web3_Instance);
         const coffeeInstance = new web3_Instance.eth.Contract(abiData,contractAddress);
         setCoffeeContract(coffeeInstance);
-
 
         //get coffee price from the contract 
         coffeeInstance.methods.ItemCost().call()
@@ -44,7 +52,8 @@ const App = () => {
 
   }, []);
 
-  function fetchPayments(){
+ async function fetchPayments(){
+   
     console.log(coffeeContract.methods.allPayments)
     coffeeContract.methods.showPayments().call()
     .then(values=>{
@@ -61,11 +70,30 @@ const App = () => {
   const buyCoffee = async () => {
     if (!web3 || !coffeeContract) return;
     const userAccounts = await web3.eth.getAccounts();
+    setAccount(userAccounts[0]);
+   
     coffeeContract.methods.makePayment(email).send({ from: userAccounts[0], value: web3.utils.toWei(coffeeCost, 'ether') })
     .on("transactionHash",hash=>{console.log("transaction hash:",hash)})
     .on("receipt",receipt=>{console.log("transaction receipt:",receipt)})
     .on("error",err=>{console.log(err)})
   }
+
+
+  let withdrawMoney=async()=>{
+
+    coffeeContract.methods.withdraw(creatorAddress).send({ from: account })
+    .on("transactionHash", hash => {
+      console.log("Withdraw transaction hash:", hash);
+    })
+    .on("receipt", receipt => {
+      console.log("Withdraw transaction receipt:", receipt);
+      // You might want to perform additional actions here after successful withdrawal
+    })
+    .on("error", err => {
+      console.error("Withdraw error:", err);
+    });
+  }
+    
 
 
   return (
@@ -100,6 +128,8 @@ const App = () => {
 
 
         }}>
+
+          {account==creatorAddress &&<button onClick={withdrawMoney}>Withdraw funds</button>}
           <h1>My Contributors</h1>
           
           <span style={{fontStyle:"italic",marginBottom:"30px"}}>(scroll to see more)</span>
@@ -108,6 +138,7 @@ const App = () => {
         <span>{purchase.email}</span>
       )
         })}
+
 
         </div>
     
